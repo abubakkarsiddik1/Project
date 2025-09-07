c15b609bb494df26c1a30abc9c02347daf5051eb
abubakkarsiddik1 <mdabubokorsiddik283@gmail.com>
2025-09-07 14:32:55 +0600

    this is fast time commit

---
+++ index.js
@@ -0,0 +1,5 @@
+  // Example JS: Button interaction
+    document.getElementById('getInvolvedBtn').addEventListener('click', () => {
+      alert("Thank you for joining the mission! 🌱");
+    });
+ 
